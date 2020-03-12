0e282f3d4900a68778f0ac9743458ced1fa56942
ravczar <ravczar@gmail.com>
2020-03-12 14:24:42 +0100

    Add Recipe detail structure html

--- UdemyApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ UdemyApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Array<Recipe> = new Array<Recipe>();
+  public recipes: Array<Recipe> = new Array<Recipe>();
 
   constructor() { 
-    this.recipes.push(new Recipe('Test recipe', 'Test recipe description', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.foodnetwork.com%2Frecipes%2Free-drummond%2F4-cheese-pepperoni-pizzadilla-7260186&psig=AOvVaw1vJ0B1ip5gj9JxyYh6FeEz&ust=1584103170166000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiT7q36lOgCFQAAAAAdAAAAABAJ' ));
+    this.recipes.push(new Recipe('Test recipe', 'Test recipe description', 'https://www.garneczki.pl/blog/wp-content/uploads/2018/07/przepis-na-sniadaniowe-placuszki.jpg' ));
+    this.recipes.push(new Recipe('Test2 recipe', 'Test2 recipe description', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'));
   }
 
   ngOnInit(): void {

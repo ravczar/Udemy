f5bdc3043f5d62edd95bf61f04ad7b3a18ba2a1f
ravczar <ravczar@gmail.com>
2020-03-12 14:56:06 +0100

    Add: ingredient model, add ingredient-list html list

--- UdemyApp/src/app/shopping-list/shopping-list.component.ts
+++ UdemyApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  public ingredients: Array<Ingredient> = new Array<Ingredient>();
 
-  constructor() { }
+  constructor() { 
+    this.ingredients.push(new Ingredient('Bread', 20));
+    this.ingredients.push(new Ingredient('Butter', 15));
+    this.ingredients.push(new Ingredient('Cornflakes', 12));
+    this.ingredients.push(new Ingredient('Milk', 18));
+    this.ingredients.push(new Ingredient('Honey', 22));
+    this.ingredients.push(new Ingredient('Apples', 3));
+  }
 
   ngOnInit(): void {
   }

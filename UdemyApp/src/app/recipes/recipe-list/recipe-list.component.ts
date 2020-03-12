6a9dff85dc74240ed3e28c17395edf67a2df96c6
ravczar <ravczar@gmail.com>
2020-03-12 13:50:19 +0100

    Add recipe list

--- UdemyApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ UdemyApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = new Array<Recipe>();
 
-  constructor() { }
+  constructor() { 
+    this.recipes.push(new Recipe('Test recipe', 'Test recipe description', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.foodnetwork.com%2Frecipes%2Free-drummond%2F4-cheese-pepperoni-pizzadilla-7260186&psig=AOvVaw1vJ0B1ip5gj9JxyYh6FeEz&ust=1584103170166000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiT7q36lOgCFQAAAAAdAAAAABAJ' ));
+  }
 
   ngOnInit(): void {
   }

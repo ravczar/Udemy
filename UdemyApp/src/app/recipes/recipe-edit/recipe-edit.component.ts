d7658b346c9924f95ebdad812c19adbf952e80d3
ravczar <ravczar@gmail.com>
2020-03-31 10:50:20 +0200

    synchornize reactive form with html code

--- UdemyApp/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ UdemyApp/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -3,6 +3,7 @@ import { RecipeService } from 'src/app/shared/services/recipe/recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from 'src/app/shared/models/recipe.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -13,6 +14,7 @@ export class RecipeEditComponent implements OnInit {
   recipe: Recipe;
   id: number;
   editMode:Boolean = false;
+  recipeForm: FormGroup;
 
   constructor(private recipeService : RecipeService,
               private router: Router, 
@@ -25,9 +27,31 @@ export class RecipeEditComponent implements OnInit {
         this.editMode = params['id'] != null;
         console.log("edit param: "+this.editMode);
         this.recipe = this.recipeService.getRecipeById(this.id);
+        // initialize form with data
+        this.initForm();
       }
     );
+  }
+  private initForm(): void{
+    let recipeName =  this.editMode ? this.recipe.name : '';
+    let recipeImagePath = this.editMode ? this.recipe.imagePath : '';
+    let recipeDescription = this.editMode ? this.recipe.description : '';
 
+    this.recipeForm = new FormGroup(
+      {
+        'name': new FormControl(recipeName),
+        'imagePath': new FormControl(recipeImagePath),
+        'description': new FormControl(recipeDescription)
+      }
+    );
+  }
+
+  onSubmit():void{
+    console.log(this.recipeForm);
+  }
+
+  getImagePath(): string{
+    return this.editMode ? this.recipe.imagePath : '';
   }
 
 }

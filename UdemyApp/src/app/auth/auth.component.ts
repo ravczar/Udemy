675ef8ffbe1990e3dd88ff71526f3fe36392cec3
ravczar <ravczar@gmail.com>
2020-04-02 21:23:16 +0200

    interact with the template driven form

--- UdemyApp/src/app/auth/auth.component.ts
+++ UdemyApp/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  @ViewChild('authForm') authForm;
+  private authForm2: NgForm;
+  public credentials: {email: string, password: string};
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onFormSubmit(form: NgForm) {
+    this.authForm2 = form;
+    console.log(this.authForm2.value);
+    this.credentials = this.authForm2.value;
+    console.log(this.credentials);
+    this.authForm2.reset();
+  }
 }

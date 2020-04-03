138e74471921401a3cd1255ecd18b1a4f860627e
ravczar <ravczar@gmail.com>
2020-04-03 22:16:37 +0200

    add UrlTree to navigate out when access restricted

--- UdemyApp/src/app/shared/services/auth-guard/auth-guard.guard.ts
+++ UdemyApp/src/app/shared/services/auth-guard/auth-guard.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../authentication/auth.service';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class AuthGuardGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.user.pipe( map(user => {
+    return this.authService.user.pipe( take(1), map(user => {
       const isAuthenticated: boolean = user ? true : false;
       if (isAuthenticated) {
         return true;

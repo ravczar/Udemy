83923e79ce3d27fe82d94c56aaef24356e3528fb
ravczar <ravczar@gmail.com>
2020-04-03 15:23:36 +0200

    error handling and display on web by ngIf

--- UdemyApp/src/app/auth/auth.component.ts
+++ UdemyApp/src/app/auth/auth.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuthResponse } from './../shared/interfaces/auth-response';
+import { Subscription } from 'rxjs';
+import { AuthService } from './../shared/services/authentication/auth.service';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
+  //@ViewChild('authForm') authForm;
+  private authForm: NgForm;
+  private apiResponse: AuthResponse;
+  private signUpSub: Subscription;
+  private signInSub: Subscription;
   isLoginMode = true;
-  @ViewChild('authForm') authForm;
-  private authForm2: NgForm;
-  public credentials: {email: string, password: string};
+  isLoading: boolean = false;
+  error: string;
 
-  constructor() { }
+  constructor( private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+    this.signUpSub.unsubscribe();
   }
 
   onSwitchMode() {
@@ -22,10 +33,49 @@ export class AuthComponent implements OnInit {
   }
 
   onFormSubmit(form: NgForm) {
-    this.authForm2 = form;
-    console.log(this.authForm2.value);
-    this.credentials = this.authForm2.value;
-    console.log(this.credentials);
-    this.authForm2.reset();
+    // extra check
+    if (form.invalid){ return; }
+    // poniższe lub viewchild
+    this.authForm = form;
+    // wyłuskanie zmiennych
+    const email = form.value.email;
+    const password = form.value.password;
+    
+    this.isLoading = true;
+    if( this.isLoginMode ) {
+      this.signInSub = this.authService.signIn(email, password)
+      .subscribe(
+        (response) => {
+          console.log("Dane logwania (signIn) z serwera:");
+          console.log(response);
+          this.apiResponse = response;
+          this.isLoading = false;
+        }, (errorMessage: string) =>{
+          console.log("niestety wystąpił błąd signIn");
+          console.log(errorMessage);
+          this.error = errorMessage;
+          this.isLoading = false;
+        }
+      );
+    } else {
+      this.signUpSub = this.authService.signUp(email, password)
+        .subscribe(
+          (response:AuthResponse) => {
+            console.log("Dane rejestracji (signUp) z serwera:");
+            console.log(response);
+            this.apiResponse = response;
+            this.isLoading = false;
+          }, 
+          (errorMessage: string) => {
+            console.log("niestety wystąpił błąd signUp")
+            console.log(errorMessage);
+            this.error = errorMessage;
+            this.isLoading = false;
+          }
+        );     
+      }
+      form.reset();
   }
+
+
 }
--- UdemyApp/src/app/shared/services/authentication/auth.service.ts
+++ UdemyApp/src/app/shared/services/authentication/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthResponse } from '../../interfaces/auth-response'
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +14,51 @@ export class AuthService {
 
   signUp( email: string, password: string){
     return this.httpClient.post<AuthResponse>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${this.APIKey}`,
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.APIKey}`,
       {
         email: email, 
         password: password,
         returnSecureToken: true
       }
-    );
+    )
+    .pipe( catchError( errorResponse => {
+      let errorMessage = 'An unknown error';
+
+      if(!errorResponse.error || !errorResponse.error.error ){
+        // e.g. network message error appeard that not suits HttpErrorResponse body
+        return throwError(errorMessage);
+      }
+      switch (errorResponse.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email exists already';
+          break;
+      }
+      return throwError(errorMessage);
+  }));
   }
+
+  signIn (email: string, password: string){
+    return this.httpClient.post<AuthResponse>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.APIKey}`,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    )
+    .pipe( catchError( errorResponse => {
+        let errorMessage = 'An unknown error';
+
+        if(!errorResponse.error || !errorResponse.error.error ){
+          return throwError(errorMessage);
+        }
+        switch (errorResponse.error.error.message) {
+          case 'INVALID_PASSWORD':
+            errorMessage = 'Please use a valid password';
+            break;
+        }
+        return throwError(errorMessage);
+    }));
+  }
+
 }
